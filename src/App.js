5ee7a86f21102f681e4749b3eb3a30d5317b14f9
medetzhan1996 <67154220+medetzhan1996@users.noreply.github.com>
2024-12-03 22:48:35 +0500

    ww

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'; // Импортируем BrowserRouter
+import { ThemeProvider } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import { LivenessQuickStartReact } from './LivenessQuickStartReact';
+import '@aws-amplify/ui-react/styles.css';
+import awsexports from './aws-exports';
 
-function App() {
+Amplify.configure(awsexports);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <BrowserRouter>
+        <LivenessQuickStartReact />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
-
-export default App;
